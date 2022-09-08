c3e6060dfb6849396bcc4287dd78a509d6f6cf49
Hiruni Perera <hiruni.p@eyepax.com>
2022-09-08 14:14:16 +0530

    signup controller

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,5 +1,18 @@
-module.exports.signup = (req, res) => {
-    res.send("new signup")
+const User = require("../models/user")
+
+module.exports.signup = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const resp = await User.create({
+            email:email,
+            password: password
+        })
+        res.status(201).json(resp)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("Error : user not created")
+    }
 }
 
 module.exports.login = (req, res) => {
