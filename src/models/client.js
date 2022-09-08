0daac3cfb716c38f2393558e04180146c57575b6
Hiruni Perera <hiruni.p@eyepax.com>
2022-09-08 13:47:35 +0530

    make email unique in client schema

--- src/models/client.js
+++ src/models/client.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose")
 const client = mongoose.model("client", new mongoose.Schema({
     email : {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     firstName : {
         type: String,
