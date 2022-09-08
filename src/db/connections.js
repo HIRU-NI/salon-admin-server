226727ef84495a50be0edd39d03386abca60d8dc
Hiruni Perera <hiruni.p@eyepax.com>
2022-09-08 12:17:12 +0530

    DB env varibale name update

--- src/db/connections.js
+++ src/db/connections.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connect = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB)
+        const conn = await mongoose.connect(process.env.MONGODB_URI)
         console.log(`connection established : ${conn.connection.host}`)
     } catch (error) {
         console.log(`Mongodb error: ${error}`)
