74f33de703e8ca3087ec00eae40d67cbd187d6bc
Hiruni Perera <hiruni.p@eyepax.com>
2022-09-08 10:40:23 +0530

    db connection url to env variable

--- src/db/connections.js
+++ src/db/connections.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose")
 
 const connect = async () => {
     try {
-        const conn = await mongoose.connect(
-            "mongodb+srv://user:pwd@cluster0.dyiywon.mongodb.net/?retryWrites=true&w=majority"
-            )
-            console.log(`connection established : ${conn.connection.host}`)
+        const conn = await mongoose.connect(process.env.MONGODB)
+        console.log(`connection established : ${conn.connection.host}`)
     } catch (error) {
         console.log(`Mongodb error: ${error}`)
     }
