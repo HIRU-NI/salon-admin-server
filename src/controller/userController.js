5dfc07f8ce57a20276e2c43e9cf584eb9ad5e34d
Hiruni Perera <hiruni.p@eyepax.com>
2022-09-26 10:18:23 +0530

    update user details

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,12 +1,46 @@
-const userModel = require("../models/user")
+const userModel = require("../models/user");
 
 module.exports.getAll = async (req, res) => {
-    try {
-        const resp = await userModel.find()
-        res.status(200).json(resp)
-    } catch (error) {
-        console.log(error)
-        const errors = handleErrors(error)
-        res.status(400).json(errors)
+  try {
+    const resp = await userModel.find();
+    res.status(200).json(resp);
+  } catch (error) {
+    console.log(error);
+    const errors = handleErrors(error);
+    res.status(400).json(errors);
+  }
+};
+
+module.exports.updateUser = async (req, res) => {
+  const id = req.params;
+
+  
+
+  const { email, firstName, lastName } = req.body;
+  try {
+    const resp = userModel.findById(id);
+
+    const user = new userModel(resp)
+
+
+    if (!user) {
+      res.status(400).json({
+        error: "User not found",
+      });
+      return;
     }
-}
+
+    user.email = email
+    user.firstName = firstName
+    user.lastName = lastName
+
+    await user.save()
+
+    res.status(200).json(user)
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({
+        error: "Server error: could not update user data"
+    })
+  }
+};
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -8,5 +8,6 @@ const { protect } = require('../middleware/authMiddleware')
 const router = express.Router()
 
 router.get("/", protect, userController.getAll) 
+router.put("/:id", protect, userController.updateUser) 
 
 module.exports = router
